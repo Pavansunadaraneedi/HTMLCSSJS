3167de30278b23dfa825edadb08c5d234fd1c98d
Pavansunadaraneedi <rockzpawan429@gmail.com>
2025-04-23 12:48:47 +0530

    modified:   JS/Datatypes/datatypes.txt 	modified:   JS/Datatypes/script.js 	new file:   JS/Operators/index.html 	new file:   JS/Operators/operator.txt 	new file:   JS/Operators/script.js

--- JS/Datatypes/script.js
+++ JS/Datatypes/script.js
@@ -17,11 +17,43 @@
 // console.log(Math.ceil(Math.random()*6))
 // console.log(Math.ceil(-3.14));
 // console.log(Math.abs(-5.96))
+// let msg= "I've understood";
+// let x= 'The gandhi said, "Good Night"';
+// let y= 'Hi, I\'am "Loki" '
+// console.log(y)
 
-let msg= "I've understood";
-let x= 'The gandhi said, "Good Night"';
-let y= 'Hi, I\'am "Loki" '
-console.log(y)
-let a= 5;
-console.log("The value is "+a+" the value of b "+b+" and sum is"+(a+b))
-console.log(` the value is ${a} the value of b is ${b} and sum is ${(a+b)}`)
+
+
+// let a = 5;
+// console.log('The value is "+a+" the value of b "+b+" and sum is +(a+b)')
+// console.log(`the value is ${a} the value of b is ${b} and sum is ${(a+b)}`)
+
+// let p = "javascript";
+// console.log(p.length) //10
+// console.log(p.toUpperCase()); //JAVASCRIPT
+// console.log(p) //javascript 
+// console.log(p.toLowerCase()); //javascript
+// console.log(p.charAt(5)) //c
+// console.log(p.indexOf('s')) //4
+// console.log(p.lastIndexOf('a')) //3
+// console.log(p.concat(" is easy"))
+// console.log(p.includes('scr'));
+// console.log(p.startsWith("ja"));
+// console.log(p.endsWith("ka")) //flase
+
+// let str= "JS is easy", p1 = "Pavan"
+// console.log(str.split(' ')) 
+// console.log(["js", "is"])
+// console.log(p1.split('a'))
+// console.log(p1.replace('a', 'k'))
+// console.log(p1.slice(2,5)) 
+
+
+
+console.log(a)
+var a = 25 ;
+console.log(a)
+let p=[5,4,3]
+p[20] = 55;
+console.log(p[4])
+
