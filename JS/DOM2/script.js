7f04c9a49a2fae93f6434f93f7f8b3c979e8c0d4
Pavansunadaraneedi <rockzpawan429@gmail.com>
2025-05-07 11:45:25 +0530

    new file:   JS/Arrays/Arrays.txt 	new file:   JS/Arrays/index.html

---
+++ JS/DOM2/script.js
@@ -0,0 +1,18 @@
+const ele=document.getElementById('heading')
+function change ()
+{
+    
+    ele.textContent = "Welcome back again "
+    ele.style.color = "red"
+}
+function changeMode()
+{
+    document.body.classList.toggle('dark')
+    document.getElementById("box")
+    style.display = "flex"
+}
+function changeMode()
+{
+    document.getElementById("box")
+    style.display = "none"
+}
