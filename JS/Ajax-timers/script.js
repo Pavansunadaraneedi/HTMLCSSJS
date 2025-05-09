39eff7da5366b209965e05741d1056e14300db46
Pavansunadaraneedi <rockzpawan429@gmail.com>
2025-05-09 12:22:11 +0530

    modified

---
+++ JS/Ajax-timers/script.js
@@ -0,0 +1,7 @@
+console.log("before timer");
+setTimeout(() => 
+{
+    console.log("Hello world");
+}, 5000)
+console.log("after timer");
+
--- JS/DOM2/script.js
+++ JS/DOM2/script.js
@@ -1,18 +1,15 @@
-const ele=document.getElementById('heading')
+const ele= document.getElementById('heading')
 function change ()
 {
-    
-    ele.textContent = "Welcome back again "
+    ele.textContent = "Welcome back again"
     ele.style.color = "red"
 }
-function changeMode()
+function changeMode ()
 {
-    document.body.classList.toggle('dark')
-    document.getElementById("box")
-    style.display = "flex"
+    // document.body.classList.toggle('dark')
+    document.getElementById("box").style.display="flex"
 }
-function changeMode()
+function closeBox ()
 {
-    document.getElementById("box")
-    style.display = "none"
+     document.getElementById("box").style.display = "none" 
 }
